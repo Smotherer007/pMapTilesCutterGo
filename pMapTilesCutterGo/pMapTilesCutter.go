44b2fbdde05e44e2459e545bd24d1d0200aa60e6
Patrick Weppelmann <pwn@sybit.de>
2022-11-28 12:54:32 +0100

    add wait group for asynchronous tiles generation

--- pMapTilesCutterGo/pMapTilesCutter.go
+++ pMapTilesCutterGo/pMapTilesCutter.go
@@ -8,87 +8,99 @@ import (
 	"log"
 	"math"
 	"os"
+	"sync"
 
 	"github.com/disintegration/imaging"
 )
 
 func CutMapIntoTiles(tileSize int, targetPath string, sourcePath string) {
+	sourceImage, sourceImageWidth, sourceImageHeight := loadImage(sourcePath)
 
-	sourceImage := loadImage(sourcePath)
-	sourceImageWidth := float64(sourceImage.Bounds().Dx())
-	sourceImageHeight := float64(sourceImage.Bounds().Dy())
+	minZoomLevel, maxZoomLevel, _ := calculateScaleParamters(sourceImage, tileSize)
 
-	maxTileDim := math.Ceil(math.Max(sourceImageWidth, sourceImageHeight) / float64(tileSize))
+	currentZoomLevel := minZoomLevel
+	currentScale := maxZoomLevel
 
-	minZoomLevel := float64(0)
-	maxZoomLevel := float64(0)
-	numTilesTotalForAllLevels := float64(1)
-	for math.Pow(2, maxZoomLevel) < maxTileDim {
-		maxZoomLevel++
-		numTilesTotalForAllLevels += math.Pow(2, 2*maxZoomLevel)
+	var tilesWaitGroup sync.WaitGroup
+	for currentZoomLevel <= maxZoomLevel {
+		canvas, canvasWidth, canvasHeight := createCanvas(currentZoomLevel, tileSize)
+		resizedImage, resizedImageWidth, resizedImageHeight := resizeImage(sourceImage, sourceImageWidth, sourceImageHeight, currentScale)
+		mergedImage := mergeImageToCanvas(canvas, canvasWidth, canvasHeight, resizedImage, resizedImageWidth, resizedImageHeight)
+		tilesWaitGroup.Add(1)
+		go func(tileSize int, targetPath string, mergedImage image.Image, canvasWidth int, canvasHeight int, currentZoomLevel int) {
+			createTiles(tileSize, targetPath, mergedImage, canvasWidth, canvasHeight, currentZoomLevel)
+			tilesWaitGroup.Done()
+		}(tileSize, targetPath, mergedImage, int(canvasWidth), int(canvasHeight), int(currentZoomLevel))
+		currentZoomLevel++
+		currentScale--
 	}
-
-	zoom := minZoomLevel
-	scale := maxZoomLevel
-	for zoom <= maxZoomLevel {
-		canvasWidth := float64(tileSize) * math.Pow(2, zoom)
-		canvasHeight := float64(tileSize) * math.Pow(2, zoom)
-		canvas := createCanvas(int(canvasWidth), int(canvasHeight), color.Black)
-
-		imageResizeWidth := scaleDimension(int(sourceImageWidth), int(scale))
-		imageResizeHeight := scaleDimension(int(sourceImageHeight), int(scale))
-		resizedImage := imaging.Resize(sourceImage, int(imageResizeWidth), int(imageResizeHeight), imaging.Linear)
-
-		top := (canvasWidth - imageResizeWidth) / 2
-		left := (canvasHeight - imageResizeHeight) / 2
-		mergedImage := imaging.Paste(canvas, resizedImage, image.Pt(int(top), int(left)))
-
-		createTiles(tileSize, targetPath, mergedImage, int(canvasWidth), int(canvasHeight), int(zoom))
-
-		zoom++
-		scale--
-	}
+	tilesWaitGroup.Wait()
 }
 
-func createTiles(tileSize int, targetPath string, mergedImage image.Image, width int, height int, zoom int) {
+func mergeImageToCanvas(canvas image.Image, canvasWidth int, canvasHeight int, imageToMerge image.Image, imageWidth int, imageHeight int) image.Image {
+	top := (canvasWidth - imageWidth) / 2
+	left := (canvasHeight - imageHeight) / 2
+	mergedImage := imaging.Paste(canvas, imageToMerge, image.Pt(int(top), int(left)))
+	return mergedImage
+}
+
+func resizeImage(image image.Image, imageWidth int, imageHeight int, currentScale int) (image.Image, int, int) {
+	width := scaleDimension(imageWidth, currentScale)
+	height := scaleDimension(imageHeight, currentScale)
+	resizedImage := imaging.Resize(image, width, height, imaging.Linear)
+	return resizedImage, width, height
+}
+
+func createTiles(tileSize int, targetPath string, mergedImage image.Image, width int, height int, currentZoomLevel int) {
 	numberOfXTiles := width / tileSize
 	numberOfYTiles := height / tileSize
-
 	for y := 0; y < numberOfYTiles; y++ {
 		tileY := y * tileSize
 		for x := 0; x < numberOfXTiles; x++ {
 			tileX := x * tileSize
-			directoryPath := fmt.Sprint(targetPath, zoom, "/", x, "/")
-			if _, err := os.Stat(directoryPath); os.IsNotExist(err) {
-				err := os.MkdirAll(directoryPath, 0755)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			upLeft := image.Pt(tileX, tileY)
-			lowRight := image.Pt(tileX+tileSize, tileY+tileSize)
-			tile := imaging.Crop(mergedImage, image.Rectangle{upLeft, lowRight})
-			saveImage(tile, fmt.Sprint(directoryPath, y, ".png"))
+			tile := createTile(mergedImage, tileX, tileY, tileSize)
+			filePath := buildFilePath(targetPath, currentZoomLevel, x, y)
+			saveImage(tile, filePath)
 		}
 	}
 }
 
-func scaleDimension(dimension int, scale int) float64 {
+func createTile(mergedImage image.Image, tileX int, tileY int, tileSize int) image.Image {
+	upLeft := image.Pt(tileX, tileY)
+	lowRight := image.Pt(tileX+tileSize, tileY+tileSize)
+	return imaging.Crop(mergedImage, image.Rectangle{upLeft, lowRight})
+}
+
+func buildFilePath(targetPath string, currentZoomLevel int, xCoordinate int, yCoordinate int) string {
+	directoryPath := fmt.Sprint(targetPath, currentZoomLevel, "/", xCoordinate, "/")
+	if _, err := os.Stat(directoryPath); os.IsNotExist(err) {
+		err := os.MkdirAll(directoryPath, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	directoryPath = fmt.Sprint(directoryPath, yCoordinate, ".png")
+	return directoryPath
+}
+
+func scaleDimension(dimension int, scale int) int {
 	scaledDimension := dimension
 	for i := 0; i < scale; i++ {
 		scaledDimension = scaledDimension / 2
 	}
-	return float64(scaledDimension)
+	return scaledDimension
 }
 
-func createCanvas(width int, height int, color color.Color) image.Image {
+func createCanvas(currentZoomLevel int, tileSize int) (image.Image, int, int) {
+	width := int(float64(tileSize) * math.Pow(2, float64(currentZoomLevel)))
+	height := int(float64(tileSize) * math.Pow(2, float64(currentZoomLevel)))
 	canvas := image.NewNRGBA(image.Rect(0, 0, width, height))
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			canvas.Set(x, y, color)
+			canvas.Set(x, y, color.Black)
 		}
 	}
-	return canvas
+	return canvas, width, height
 }
 
 func saveImage(image image.Image, imagePath string) {
@@ -98,10 +110,28 @@ func saveImage(image image.Image, imagePath string) {
 	}
 }
 
-func loadImage(imagePath string) image.Image {
+func loadImage(imagePath string) (image.Image, int, int) {
 	image, err := imaging.Open(imagePath, imaging.AutoOrientation(true))
 	if err != nil {
 		log.Fatal(err)
 	}
-	return image
+	width := float64(image.Bounds().Dx())
+	height := float64(image.Bounds().Dy())
+	return image, int(width), int(height)
+}
+
+func calculateScaleParamters(sourceImage image.Image, tileSize int) (int, int, int) {
+	sourceImageWidth := float64(sourceImage.Bounds().Dx())
+	sourceImageHeight := float64(sourceImage.Bounds().Dy())
+
+	maxTileDim := math.Ceil(math.Max(sourceImageWidth, sourceImageHeight) / float64(tileSize))
+
+	minZoomLevel := float64(0)
+	maxZoomLevel := float64(0)
+	numberOfTiles := float64(1)
+	for math.Pow(2, maxZoomLevel) < maxTileDim {
+		maxZoomLevel++
+		numberOfTiles += math.Pow(2, 2*maxZoomLevel)
+	}
+	return int(minZoomLevel), int(maxZoomLevel), int(numberOfTiles)
 }
